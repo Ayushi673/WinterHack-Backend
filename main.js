723193d9f798b496a2c6a022b84255e25b981a4d
Ayushi673 <dasayushi16@gmail.com>
2022-01-02 19:56:22 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+const {MongoClient} = require("mongodb");
+const Express = require("express");
+const Cors = require("cors");
+require('dotenv').config();
+const app = Express();
+
+app.use(Cors());
+app.use(Express.json());
+
+
+const client = new MongoClient(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+var collection;
+
+app.post("/search", async(req,res)=>{
+    try{
+        let result = await collection.aggregate([
+            {
+                "$search":{
+                    "index": "autocomplete",
+                    "compound": {
+                        "must": [
+                            {
+                                "autocomplete": {
+                                    "query": `${req.body.query}`,
+                                    "path": "name"
+                                }
+                            },
+                            {
+                                "geoWithin":{
+                                    "circle":{
+                                        "center":{
+                                            "type":"Point",
+                                            "coordinates": [req.body.position.lng,req.body.position.lat]
+                                        },
+                                        "radius": 10000
+                                    },
+                                    "path":"address.location"
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        ]).toArray();
+        res.send(result);
+    } catch(err){
+        res.status(500).send({ message: err.message, statusCode: 500});
+    }
+})
+let port = process.env.PORT || 3000;
+app.listen(port, async()=>{
+    try{
+        await client.connect();
+        collection= client.db("sample_airbnb").collection("listingsAndReviews");
+    }catch(err){
+        console.log(err);                                                                                                           
+    }
+})
